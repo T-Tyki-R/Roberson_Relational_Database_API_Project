aa81d783c8020c1597d09e9271b1e4373fdb0aae
Tameka Roberson <tamekaroberson1995@gmail.com>
2025-05-13 19:48:37 -0400

    Inital Commit

---
+++ app.py
@@ -0,0 +1,39 @@
+# Import Packages
+
+# ______ Initialize Flask App ______ 
+
+
+# ______ Initialize Table(s) ______ 
+# Create a table for each of the following:
+# 1. Users
+# 2. Products
+# 3. Orders
+# 4. Order Product Association
+
+
+# ______ Initialize Route(s) ______
+# Create a route for each of the following:
+# User Endpoints
+# 1. GET/users
+# 2. GET/users/<user_id>
+# 3. POST/users
+# 4. PUT/users/<user_id>
+# 5. DELETE/users/<user_id>
+
+
+# Product Endpoints
+# 1. GET/products
+# 2. GET/products/<product_id>
+# 3. POST/products
+# 4. PUT/products/<product_id>
+# 5. DELETE/products/<product_id>
+
+
+# Order Endpoints
+# 1. GET/orders/user/<user_id>: Get all orders for a user
+# 2. GET/orders/<order_id>/products: Get all products in an order
+# 3. POST/orders
+# 4. PUT /orders/<order_id>/add_product/<product_id>: Add a product to an order (prevent duplicates)
+# 5. DELETE/orders/<order_id>/remove_product: Remove a product from an order
+
+
